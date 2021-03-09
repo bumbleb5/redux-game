8488947dbda977d391f886c37f90d146f8d25f00
bumbleb5 <69547355+bumbleb5@users.noreply.github.com>
2021-03-09 14:37:03 -0600

    adding buying and selling

--- game.js
+++ game.js
@@ -32,6 +32,26 @@ const initialWagonState = {
           days: state.days + 1
         };
       };
+      case 'sell': {
+        return {
+          ...state,
+          cash: state.cash + 5,
+          supplies: state.supplies - 20
+        };
+      };
+      case 'buy': {
+        return {
+          ...state,
+          cash: state.cash - 15,
+          supplies: state.supplies + 25
+        };
+      };
+      case 'theft': {
+        return {
+          ...state,
+          cash: Math.floor(state.cash / 2)
+        };
+      };
       default: {
         return state;
       };
@@ -62,4 +82,20 @@ const initialWagonState = {
   //can't travel anymore, insufficient supplies!
   wagon = wagonReducer(wagon, {type: 'travel', payload: 1});
   console.log(wagon);
+
+  //up and going, gathered for a few days
+wagon = wagonReducer(wagon, {type: 'gather'});
+console.log(wagon);
+wagon = wagonReducer(wagon, {type: 'gather'});
+console.log(wagon);
+wagon = wagonReducer(wagon, {type: 'gather'});
+console.log(wagon);
+
+//sell some supplies
+wagon = wagonReducer(wagon, {type: 'sell'});
+console.log(wagon);
+
+//got robbed
+wagon = wagonReducer(wagon, {type: 'theft'});
+console.log(wagon);
   
